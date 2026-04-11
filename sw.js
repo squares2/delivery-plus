efca553a49b821a0c3d1c851574b055f714e8ca3
squares2 <strategyquant8@gmail.com>
2026-04-11 23:37:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
 self.addEventListener('fetch', (event) => {
-    // This allows the app to load the pages from the network 
-    // even if it's running in standalone mode.
-    event.respondWith(fetch(event.request));
+  // Add this line to ignore Netlify functions
+  if (event.request.url.includes('/.netlify/functions/')) {
+    return; // Let the browser handle the request normally
+  }
+  
+  // ... rest of your service worker code
 });
